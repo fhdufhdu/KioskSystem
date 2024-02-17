a0d0359989c9121e749878b8307124825ec11e55
fhdufhdu <fhdufhdu@gmail.com>
2024-02-17 19:12:22 +0900

    first commit

---
+++ src/main/kotlin/com/fhdufhdu/movie/MovieReservationSystemApplication.kt
@@ -0,0 +1,11 @@
+package com.fhdufhdu.movie
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class MovieReservationSystemApplication
+
+fun main(args: Array<String>) {
+	runApplication<MovieReservationSystemApplication>(*args)
+}
---
+++ src/test/kotlin/com/fhdufhdu/movie/MovieReservationSystemApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.fhdufhdu.movie
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class MovieReservationSystemApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
