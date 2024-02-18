322d7ef9e3556c8a4e0ce374c5f56d45f249b8a8
fhdufhdu <fhdufhdu@gmail.com>
2024-02-18 17:08:43 +0900

    feat: erd 추가

---
+++ src/main/kotlin/com/fhdufhdu/kiosk/config/SpringConfiguration.kt
@@ -0,0 +1,8 @@
+package com.fhdufhdu.kiosk.config
+
+import org.springframework.context.annotation.ComponentScan
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@ComponentScan
+class SpringConfiguration {}
