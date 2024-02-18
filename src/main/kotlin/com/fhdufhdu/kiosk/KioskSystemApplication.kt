f83722ec7b378fb9057e13d25c11bd2fb8808611
fhdufhdu <fhdufhdu@gmail.com>
2024-02-18 15:18:19 +0900

    feat: movie -> kiosk로 변경

--- src/main/kotlin/com/fhdufhdu/kiosk/KioskSystemApplication.kt
+++ src/main/kotlin/com/fhdufhdu/kiosk/KioskSystemApplication.kt
@@ -1,11 +1,11 @@
-package com.fhdufhdu.movie
+package com.fhdufhdu.kiosk
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class MovieReservationSystemApplication
+class KioskSystemApplication
 
 fun main(args: Array<String>) {
-	runApplication<MovieReservationSystemApplication>(*args)
+	runApplication<KioskSystemApplication>(*args)
 }
